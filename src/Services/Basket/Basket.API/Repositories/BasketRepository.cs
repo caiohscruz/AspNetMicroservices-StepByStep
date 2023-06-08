1f6d0b3b48eff7848fb4abb71d60ed903e1bded3
CaioHSCruz <caiohscruz@hotmail.com>
2023-06-08 11:13:53 -0300

    Add Basket.API

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using ServiceStack.Redis;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IRedisClient _redisClient;
+
+        public BasketRepository(IRedisClientsManager redisClientsManager)
+        {
+            _redisClient = redisClientsManager.GetClient();
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var json = _redisClient.GetValue(userName);
+            return string.IsNullOrEmpty(json) ? null : JsonSerializer.Deserialize<ShoppingCart>(json);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            var json = JsonSerializer.Serialize(basket);
+            _redisClient.SetValue(basket.UserName, json);
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            _redisClient.Remove(userName);
+        }
+    }
+}
