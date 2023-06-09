f96b10b531268b53f5b389a2af76c498eb57356b
CaioHSCruz <caiohscruz@hotmail.com>
2023-06-09 17:07:19 -0300

    Add Discount.Grpc

---
+++ src/Services/Discount/Discount.Grpc/Mapper/AutoMapperConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Discount.Grpc.Mapper
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Discount.Grpc.Mapper.AutoMapperConfiguration));
+            AutoMapperConfiguration.RegisterMappings();
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Mapper/AutoMapperConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Discount.Grpc.Mapper
+{
+    public class AutoMapperConfiguration
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new DiscountProfile());
+            });
+        }
+
+    }
+}
