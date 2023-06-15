feb9667c5864abf473882e61bbd3ea3170b8267c
CaioHSCruz <caiohscruz@hotmail.com>
2023-06-14 21:46:15 -0300

    Add RabbitMQ and Publisher

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,10 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using ServiceStack;
 using ServiceStack.Redis;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,14 +15,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Redis Configuration
 builder.Services.AddSingleton<IRedisClientsManager>(c =>
 {
     var redisConnectionString = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
     return new RedisManagerPool(redisConnectionString);
 });
 
+// General Configuration
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+// AutoMapper Configuration
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+// Grpc Configuration
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     options =>
     {
@@ -28,6 +36,14 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
         options.Address = new Uri(discountUrl);
     });
 builder.Services.AddScoped<DiscountGrpcService>();
+
+// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) => {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+    });
+});
 
 var app = builder.Build();
 
