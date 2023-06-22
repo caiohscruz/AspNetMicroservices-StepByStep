9e39ee8de2f18c38034c40de99a8ea196fc5bdc1
CaioHSCruz <caiohscruz@hotmail.com>
2023-06-21 21:15:41 -0300

    Add Serilog

--- src/WebApps/AspNetRunBasics/Program.cs
+++ src/WebApps/AspNetRunBasics/Program.cs
@@ -1,18 +1,52 @@
 using AspnetRunBasics.Services;
+using Serilog;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
+#pragma warning disable CS8604 // Possible null reference argument.
                 c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
+#pragma warning restore CS8604 // Possible null reference argument.
 
 builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+#pragma warning disable CS8604 // Possible null reference argument.
     c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
+#pragma warning restore CS8604 // Possible null reference argument.
 
 builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));    
+#pragma warning disable CS8604 // Possible null reference argument.
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
+#pragma warning restore CS8604 // Possible null reference argument.
 
 builder.Services.AddRazorPages();
+
+builder.Host.UseSerilog((ctx, config) =>
+{
+    var appName = Assembly.GetExecutingAssembly().GetName().Name;
+    var environment = ctx.HostingEnvironment.EnvironmentName;
+
+    config
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(
+#pragma warning disable CS8604 // Possible null reference argument.
+            new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri(ctx.Configuration["ElasticConfiguration:Uri"]))
+#pragma warning restore CS8604 // Possible null reference argument.
+            {
+                IndexFormat = $"applogs-{appName?.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-logs-{DateTime.UtcNow:yyyy-MM}",
+                AutoRegisterTemplate = true,
+                NumberOfShards = 3,
+                NumberOfReplicas = 1,
+            }
+        )
+#pragma warning disable CS8604 // Possible null reference argument.
+        .Enrich.WithProperty("Environment", environment)
+#pragma warning restore CS8604 // Possible null reference argument.
+        .ReadFrom.Configuration(ctx.Configuration);
+});
 
 var app = builder.Build();
 
--- src/WebApps/AspNetRunBasics/Services/CatalogService.cs
+++ src/WebApps/AspNetRunBasics/Services/CatalogService.cs
@@ -10,11 +10,13 @@ namespace AspnetRunBasics.Services
 {
     public class CatalogService : ICatalogService
     {
-        private readonly HttpClient _client;        
-
+        private readonly HttpClient _client;
+        private readonly ILogger<CatalogService> _logger;
+        
         public CatalogService(HttpClient client, ILogger<CatalogService> logger)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task<IEnumerable<CatalogModel>> GetCatalog()
@@ -31,12 +33,14 @@ namespace AspnetRunBasics.Services
 
         public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
         {
+            _logger.LogInformation("Getting Catalog Products from url: {url} and custom property : {customProperty}", _client.BaseAddress, 6);
+
             var response = await _client.GetAsync($"/Catalog/GetProductByCategory/{category}");
             return await response.ReadContentAs<List<CatalogModel>>();
         }
 
         public async Task<CatalogModel> CreateCatalog(CatalogModel model)
-        {            
+        {
             var response = await _client.PostAsJson($"/Catalog", model);
             if (response.IsSuccessStatusCode)
                 return await response.ReadContentAs<CatalogModel>();
@@ -44,6 +48,6 @@ namespace AspnetRunBasics.Services
             {
                 throw new Exception("Something went wrong when calling api.");
             }
-        }        
+        }
     }
 }
